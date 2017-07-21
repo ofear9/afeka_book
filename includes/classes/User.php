8316fae08b8ae3a625eac2520ccb16332f54b203
OPHIRDELL\ofear <ofear9@gmail.com>
2017-07-21 14:23:11 +0300

    last update

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -31,6 +31,27 @@ class User {
 	}
 
 
+	public function isClosed(){
+		$username = $this->user['username'];
+		$query = mysqli_query($this->con, "SELECT user_closed FROM 	users 	WHERE username='$username'");
+		$row = mysqli_fetch_array($query);
+		if($row['user_closed'] == 'yes')
+			return true;
+		else
+			return false;
+	}
+
+	public function isFriend($username_to_check){
+		$usernameComma = "," . $username_to_check . ",";
+
+		if((strstr($this->user['friend_array'],$usernameComma) || $username_to_check == $this->user['username'])) {
+			return true;
+		}
+		else
+			return false;
+	}
+
+
 
 
 
