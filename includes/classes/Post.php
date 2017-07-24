4c219d32f4d0b4caaf070d1bc1e4ba9248a1fe47
OPHIRDELL\ofear <ofear9@gmail.com>
2017-07-24 17:17:04 +0300

    small changes in post.php - add more padding between the posted_by and the body

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -195,7 +195,7 @@ class Post {
 						<img src='$profile_pic' width='50'>
 					</div>
 
-					<div class='posted_by' style='color:#ACACAC;'>
+					<div class='posted_by' style='color:#ACACAC; padding:0px 0px 5px 0px;'>
 						<a href='$added_by'> $first_name $last_name </a> $user_to &nbsp;&nbsp;&nbsp;&nbsp;$time_message
 					</div>
 					<div id='post_body'>
